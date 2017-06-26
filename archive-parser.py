239d9b6b53c442ccc02dfbc1d06fbb8b01630198
alex <alexost82@gmail.com>
2017-06-26 14:25:58 +0200

    adding slash for each folder

--- archive-parser.py
+++ archive-parser.py
@@ -1,16 +1,16 @@
-import zipfile, tarfile, os, tempfile, sys
+import zipfile, tarfile, os, sys, tempfile
 
 def list_all_nested_files(path_to_archive, prefix=''):
-    if prefix == '':
-        prefix = path_to_archive+"/"
+    if prefix == '': prefix = path_to_archive+"/"
     nested_files_list = []
     #processing tar
     if tarfile.is_tarfile(path_to_archive):
         t = tarfile.open(path_to_archive)
         for i in t.getnames():
-            nested_files_list.append(prefix+i)
             if t.getmember(i).isdir():
+                nested_files_list.append(prefix+i+"/")
                 continue
+            nested_files_list.append(prefix+i)
             fd, nested_file_path = tempfile.mkstemp()
             os.write(fd, t.extractfile(i).read())
             if zipfile.is_zipfile(nested_file_path) or tarfile.is_tarfile(nested_file_path):
@@ -21,7 +21,7 @@ def list_all_nested_files(path_to_archive, prefix=''):
     elif zipfile.is_zipfile(path_to_archive):
         z = zipfile.ZipFile(path_to_archive)
         for i in z.namelist():
-            nested_files_list.append(prefix+i.rstrip("/"))
+            nested_files_list.append(prefix+i)
             fd, nested_file_path = tempfile.mkstemp()
             os.write(fd, z.read(i))
             if zipfile.is_zipfile(nested_file_path) or tarfile.is_tarfile(nested_file_path):

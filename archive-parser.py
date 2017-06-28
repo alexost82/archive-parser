21516806a9d0b834765ec6184f16128dcc78ddd5
alex <alexost82@gmail.com>
2017-06-28 11:43:09 +0200

    code refactoring

--- archive-parser.py
+++ archive-parser.py
@@ -1,26 +1,11 @@
 import zipfile, tarfile, os, sys, tempfile
 
+
 def list_all_nested_files(path_to_target, prefix=''):
-    path_to_target = path_to_target.rstrip("/")
-    if prefix == '': prefix = path_to_target+"/"
-    nested_files_list = []
-    #processing folder
-    if os.path.isdir(path_to_target):
-        for i in os.listdir(path_to_target):
-            direct_path = path_to_target + "/" + i
-            nested_files_list.append(direct_path)
-            if os.path.isdir(direct_path):
-                index = nested_files_list.index(direct_path)
-                nested_files_list[index] = direct_path + "/"
-                new_list = list_all_nested_files(direct_path)
-                nested_files_list.extend(new_list)
-                continue                         
-            if zipfile.is_zipfile(direct_path) or tarfile.is_tarfile(direct_path):
-                new_list = list_all_nested_files(i, prefix+i+"/")
-                nested_files_list.extend(new_list)
-    #processing tar
-    elif tarfile.is_tarfile(path_to_target):
-        t = tarfile.open(path_to_target)
+    
+    def tar_processing(path_to_tar):
+        print "TAR processed", path_to_tar
+        t = tarfile.open(path_to_tar)
         for i in t.getnames():
             if t.getmember(i).isdir():
                 nested_files_list.append(prefix+i+"/")
@@ -32,8 +17,9 @@ def list_all_nested_files(path_to_target, prefix=''):
                 new_list = list_all_nested_files(nested_file_path, prefix+i+"/")
                 nested_files_list.extend(new_list)
             os.close(fd)
-    #processing zip
-    elif zipfile.is_zipfile(path_to_target):
+
+    def zip_processing(path_to_zip):
+        print 'ZIP processed', path_to_target
         z = zipfile.ZipFile(path_to_target)
         for i in z.namelist():
             nested_files_list.append(prefix+i)
@@ -43,6 +29,33 @@ def list_all_nested_files(path_to_target, prefix=''):
                 new_list = list_all_nested_files(nested_file_path, prefix+i+"/")
                 nested_files_list.extend(new_list)
             os.close(fd)
+
+    path_to_target = path_to_target.rstrip("/")
+    if prefix == '': prefix = path_to_target+"/"
+    nested_files_list = []
+    print prefix, path_to_target, 'starting'
+    #processing folder
+    if os.path.isdir(path_to_target):
+        for i in os.listdir(path_to_target):
+            direct_path = path_to_target + "/" + i
+            print direct_path, 'direct'
+            nested_files_list.append(direct_path)
+            if os.path.isdir(direct_path):
+                index = nested_files_list.index(direct_path)
+                nested_files_list[index] = direct_path + "/"
+                new_list = list_all_nested_files(direct_path)
+                nested_files_list.extend(new_list)
+                continue
+            elif os.path.islink(direct_path):
+                continue
+            elif os.path.getsize(direct_path) == 0:
+                continue
+            elif zipfile.is_zipfile(direct_path) or tarfile.is_tarfile(direct_path):
+                new_list = list_all_nested_files(direct_path, prefix+i+"/")
+                nested_files_list.extend(new_list)
+                    
+    elif tarfile.is_tarfile(path_to_target): tar_processing(path_to_target)
+    elif zipfile.is_zipfile(path_to_target): zip_processing(path_to_target)
     
     else:
         print('{} is not an acceptable archive file or folder'.format(path_to_target))
